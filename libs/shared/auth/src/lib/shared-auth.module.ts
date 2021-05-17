de5ea2e7d773a409c0666f401215330b1bb6e5c7
Victor H Bazan M <vhbazanm@gmail.com>
2021-05-17 16:49:00 +0200

    Add ngrx to auth lib

--- libs/shared/auth/src/lib/shared-auth.module.ts
+++ libs/shared/auth/src/lib/shared-auth.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromAuth from './+state/auth.reducer';
+import { AuthEffects } from './+state/auth.effects';
 
 @NgModule({
   imports: [
@@ -8,6 +12,8 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+    StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects]),
   ],
 })
 export class SharedAuthModule {}
