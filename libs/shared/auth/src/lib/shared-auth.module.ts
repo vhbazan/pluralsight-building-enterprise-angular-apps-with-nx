7bf065eb08f6161a8b967b598fc3ac46d951b38b
Victor H Bazan M <vhbazanm@gmail.com>
2021-05-18 10:13:08 +0200

    Add login container component

--- libs/shared/auth/src/lib/shared-auth.module.ts
+++ libs/shared/auth/src/lib/shared-auth.module.ts
@@ -5,6 +5,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import * as fromAuth from './+state/auth.reducer';
 import { AuthEffects } from './+state/auth.effects';
+import { LoginComponent } from './containers/login/login.component';
 
 @NgModule({
   imports: [
@@ -14,6 +15,9 @@ import { AuthEffects } from './+state/auth.effects';
     ]),
     StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer),
     EffectsModule.forFeature([AuthEffects]),
+  ],
+  declarations: [
+    LoginComponent
   ],
 })
 export class SharedAuthModule {}
